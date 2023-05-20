ac4b7de623792ad198f00896afe7b6efa7c9424a
Addison <addison@Rhys.local>
2023-05-20 00:43:30 -0700

    mostly done

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useState } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [time, setTime] = useState("")
+  const [textColor, setTextColor] = useState("")
+
+  function getRGB(c: any) {
+    return parseInt(c, 16) || c
+  }
+
+  function getsRGB(c: any) {
+    return getRGB(c) / 255 <= 0.03928
+      ? getRGB(c) / 255 / 12.92
+      : Math.pow((getRGB(c) / 255 + 0.055) / 1.055, 2.4)
+  }
+
+  function getLuminance(hexColor: any) {
+    return (
+      0.2126 * getsRGB(hexColor.substr(1, 2)) +
+      0.7152 * getsRGB(hexColor.substr(3, 2)) +
+      0.0722 * getsRGB(hexColor.substr(-2))
+    )
+  }
+
+  function getContrast(f: string, b: string) {
+    const L1 = getLuminance(f)
+    const L2 = getLuminance(b)
+    return (Math.max(L1, L2) + 0.05) / (Math.min(L1, L2) + 0.05)
+  }
+
+  const getTextColor = useCallback((bgColor: string) => {
+    const whiteContrast = getContrast(bgColor, '#ffffff')
+    const blackContrast = getContrast(bgColor, '#000000')
+
+    setTextColor(whiteContrast > blackContrast ? '#ffffff' : '#000000')
+    // return whiteContrast > blackContrast ? '#ffffff' : '#000000'
+  }, [])
+
+  useState(() => {
+
+
+    function getTime() {
+      const date = new Date()
+
+      const hour = date.getHours() < 10 ? "0" + date.getHours().toString() : date.getHours().toString()
+      const minute = date.getMinutes() < 10 ? "0" + date.getMinutes().toString() : date.getMinutes().toString()
+      const second = date.getSeconds() < 10 ? "0" + date.getSeconds().toString() : date.getSeconds().toString()
+
+      const bgColor = hour + minute + second
+
+      setTime(bgColor)
+      getTextColor(bgColor)
+
+    }
+
+
+    setInterval(function () {
+      getTime()
+    }, 1000)
+
+    getTime()
+
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main style={{ backgroundColor: "#" + time, color: textColor }}>
+      <div className="container">
+        <h1>Current time: #{time}</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
